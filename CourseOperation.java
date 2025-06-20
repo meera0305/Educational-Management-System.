12c1cf84e395d7e8174a63cda195052d48b531ca
meera0305 <chavanmeera66@gmail.com>
2025-06-20 12:00:26 +0530

    Create CourseOperation.java

---
+++ CourseOperation.java
@@ -0,0 +1,36 @@
+import java.sql.*;
+import java.util.Scanner;
+
+public class CourseOperations {
+    public static void addCourse(Scanner sc) {
+        System.out.print("Course ID: ");
+        int id = sc.nextInt(); sc.nextLine();
+        System.out.print("Course Title: ");
+        String title = sc.nextLine();
+
+        try (Connection con = DBConnection.getConnection()) {
+            String sql = "INSERT INTO courses(id,title) VALUES(?,?)";
+            PreparedStatement pst = con.prepareStatement(sql);
+            pst.setInt(1, id);
+            pst.setString(2, title);
+            pst.executeUpdate();
+            System.out.println("✔️ Course added!");
+        } catch (SQLException e) {
+            System.err.println("Add Error: " + e);
+        }
+    }
+
+    public static void viewCourses() {
+        System.out.println("Course ID | Title");
+        try (Connection con = DBConnection.getConnection()) {
+            ResultSet rs = con.createStatement()
+                .executeQuery("SELECT * FROM courses");
+            while (rs.next()) {
+                System.out.printf("%d | %s\n",
+                    rs.getInt("id"), rs.getString("title"));
+            }
+        } catch (SQLException e) {
+            System.err.println("View Error: " + e);
+        }
+    }
+}
