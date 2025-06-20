9953ab65dcc440a6ba193141e44d6a1f3fcb03f0
meera0305 <chavanmeera66@gmail.com>
2025-06-20 11:59:45 +0530

    Create Course.java

---
+++ Course.java
@@ -0,0 +1,12 @@
+public class Course {
+    private int id;
+    private String title;
+
+    public Course(int id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public int getId() { return id; }
+    public String getTitle() { return title; }
+}
