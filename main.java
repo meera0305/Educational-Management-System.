f853711c7f8e3a4226510893679c2fffd8752918
meera0305 <chavanmeera66@gmail.com>
2025-06-24 15:41:25 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Student student = new Student();
+
+        while (true) {
+            System.out.println("\n----- Educational Management System -----");
+            System.out.println("1. Add Student");
+            System.out.println("2. View Students");
+            System.out.println("3. Exit");
+            System.out.print("Enter choice: ");
+            int choice = sc.nextInt();
+
+            switch (choice) {
+                case 1:
+                    student.addStudent();
+                    break;
+                case 2:
+                    student.viewStudents();
+                    break;
+                case 3:
+                    System.out.println("Thank you!");
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid choice. Try again.");
+            }
+        }
+    }
+}
